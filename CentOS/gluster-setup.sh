3238f1dd4a5922f65536c7668adff0eae02d0011
jeff@gsat.us <jeff@gsat.us>
2021-01-07 06:18:20 +0000

    cleanup errant line in setup script

--- CentOS/gluster-setup.sh
+++ CentOS/gluster-setup.sh
@@ -19,8 +19,6 @@ main () {
   GLUSTERFS_LOG_CONT_DIR="/var/log/glusterfs/container"
   GLUSTERFS_CUSTOM_FSTAB="/var/lib/heketi/fstab"
 
-  $(ls -1q /etc/glusterfs | wc -l) eq 1
-
   # Handle case where startup image already has a single file gsyncd.conf in
   # /etc/glusterfs which stops the initial sync from running properly. Check if
   # there is only 1 file in the /etc/glusterfs directory, and delete it if so. 
